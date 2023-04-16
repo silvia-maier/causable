a8ed32e0c37679aab439d2dd3a921b7132560fb9
Lukas <lpeyer@gmail.com>
2023-04-16 20:02:21 +0200

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html", "./**/*.html"],
+  theme: {
+    extend: {
+      backgroundColor: {
+        'primary': '#3AA1BA',
+        'secondary': '#C12C90',
+        'tertiary': '#1D416D',
+        'dark': '#0B1828',
+        'neutral': '#F4F2F0'
+      },
+      textColor: {
+        'primary': '#3AA1BA',
+        'secondary': '#C12C90',
+        'tertiary': '#1D416D',
+        'neutral': '#F4F2F0'
+      },
+      fontFamily: {
+        'serif': ['Lora', 'serif'],
+        'sans': ['Ubuntu', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
+
