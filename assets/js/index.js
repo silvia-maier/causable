17ac12cda482f9ea2b407063bada727eed4b510f
Lukas <lpeyer@gmail.com>
2023-10-10 22:36:56 +0200

    feat: new site

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,10 +1,98 @@
-const elem = document.querySelector('.eml')
-const name = elem.getAttribute('data-x')
-const host = elem.getAttribute('data-y')
-const mail = name + '@' + host
+document.addEventListener("DOMContentLoaded", function() {
 
-const link = document.createElement('a')
-link.setAttribute('href', 'mailto:' + mail)
-link.textContent = mail
+  // Burger Menu
+  // -----------
+  const menuBtn = document.querySelector('.js-menu-btn')
+  const mobileMenu = document.querySelector('#mobile-menu')
 
-elem.parentElement.replaceChild(link, elem)
+  const openMenu = () => {
+    // check if mobile menu is empty ul is empty
+    if (mobileMenu.querySelectorAll('li').length === 1) {
+      const ulElem = mobileMenu.querySelector('ul')
+      const menu = document.querySelector('#menu')
+      const menuItems = menu.querySelectorAll('li')
+      // copy all <li> elements in the first <ul> element
+      // copy the elements and insert them into the ul inside the mobile menu
+      menuItems.forEach(item => {
+        ulElem.appendChild(item.cloneNode(true))
+      })
+    }
+
+    mobileMenu.classList.remove('hidden')
+    menuBtn.classList.add('tham-active')
+
+    // add event listener in next tick, otherwise it picks up the current click event
+    setTimeout(() => {
+      document.addEventListener('click', menuClickHandler)
+      window.addEventListener('scroll', menuScrollHandler)
+    }, 0)
+  }
+
+  const closeMenu = () => {
+    document.removeEventListener('click', menuClickHandler)
+    document.removeEventListener('scroll', menuScrollHandler)
+    scrollCount = 0
+    mobileMenu.classList.add('hidden')
+    menuBtn.classList.remove('tham-active')
+  }
+
+  const menuClickHandler = (event) => {
+    if (event.target.closest('#mobile-menu') === null) closeMenu()
+  }
+
+  let scrollCount = 0
+  const menuScrollHandler = (event) => {
+    scrollCount += 1
+    if (scrollCount > 2) closeMenu()
+  }
+
+  menuBtn.addEventListener('click', () => {
+    if (mobileMenu.classList.contains('hidden')) {
+      openMenu()
+    } else {
+      closeMenu()
+    }
+  })
+
+  // Contact form
+  const contactSections = document.querySelectorAll('.js-contact')
+  // register click handler for all
+  contactSections.forEach(contactSection => {
+    const contactBtn = contactSection.querySelector('.js-contact-btn')
+    contactBtn.addEventListener('click', () => {
+      const contactForm = contactSection.querySelector('.js-contact-form')
+      contactForm.classList.toggle('hidden')
+    })
+  })
+
+  // Shrinking Header
+  // ----------------
+  const header = document.querySelector('header')
+
+  let lastScrollY = 0;
+  window.addEventListener('scroll', () => {
+    const currentScrollY = window.scrollY
+    if (currentScrollY > lastScrollY) {
+      header.classList.add('header-shrink')
+    } else {
+      header.classList.remove('header-shrink')
+    }
+    lastScrollY = currentScrollY
+  })
+
+  // Contact
+  // -------
+  const elem = document.querySelector('.eml')
+  const name = elem.getAttribute('data-x')
+  const host = elem.getAttribute('data-y')
+  const mail = name + '@' + host
+
+  const link = document.createElement('a')
+  link.setAttribute('href', 'mailto:' + mail)
+  link.textContent = mail
+
+  elem.parentElement.replaceChild(link, elem)
+})
+
+
+
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,25 +3,50 @@ module.exports = {
   content: ["./*.html", "./**/*.html"],
   theme: {
     extend: {
+      colors: {
+        'clay3': '#DA928A',
+        'secondary3': '#D50058'
+      },
       backgroundColor: {
-        'primary': '#3AA1BA',
-        'secondary': '#C12C90',
-        'tertiary': '#1D416D',
-        'dark': '#0B1828',
-        'neutral': '#F4F2F0'
+        'white': '#FFFCFC',
+        'grey1': '#ECECEC',
+        'grey4': '#646464',
+        'black': '#000000',
+        'primary1': '#CCDFF1',
+        'primary2': '#005DB9',
+        'primary3': '#004A88',
+        'primary4': '#0C3B5D',
+        'secondary1': '#FCEBF2',
+        'secondary2': '#F7CCDE',
+        'secondary3': '#D50058',
+        'secondary4': '#701D45',
+        'clay1': '#F9F1EF',
+        'clay2': '#E4BAB4',
+        'clay3': '#DA928A',
+        'clay4': '#C76D61',
+        'sage1': '#BECEC3',
+        'sage2': '#A7BDB4',
+        'sage3': '#7D9C91',
+        'sage4': '#163029'
       },
       textColor: {
-        'primary': '#3AA1BA',
-        'secondary': '#C12C90',
-        'tertiary': '#1D416D',
-        'neutral': '#F4F2F0'
+        'primary3': '#004A88',
+        'primary4': '#0C3B5D',
+        'white': '#FFFCFC',
+        'black': '#000000',
+        'dark': '#646464',
+        'link': '#C12C90'
       },
       fontFamily: {
-        'serif': ['Lora', 'serif'],
-        'sans': ['Roboto', 'sans-serif'],
+        'serif': ['Crimson Pro', 'serif'],
+        'sans': ['StabilGrotesk', 'Roboto', 'sans-serif'],
       },
+      rotate: {
+        '-25': '-25deg',
+        '-30': '-30deg'
+       }
     },
   },
-  plugins: [],
+  plugins: [require('tailwind-hamburgers')]
 }
 
